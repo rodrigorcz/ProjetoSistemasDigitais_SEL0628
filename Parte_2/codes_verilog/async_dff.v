6cc26d885946b91d852b0c544f71aa26d8f807de
RodrigoRCZ <rodrigo.rcastro27@gmail.com>
2023-07-19 21:24:29 -0300

    update

--- Parte_2/codes_verilog/async_dff.v
+++ Parte_2/codes_verilog/async_dff.v
@@ -1,24 +1,30 @@
-module d_ff(output reg q, qb, input clk, clrn, d);
-  always @ (negedge clk or posedge clrn)
-	if (clrn)
-		q = 1'b0;
-	else
-		q = d;
-	assign qb = ~q;
+module d_ff#(parameter N = 10, parameter width = 4)(output reg[width-1:0] q, qb, input clk, clrn, d);
+  always @ (negedge clk or posedge clrn) begin
+    if (clrn) begin
+      q = 0;
+    end
+	else begin
+      if (q == N-1)
+        q = 0;
+      else
+        q = d;
+    end
+  end
+  assign qb = ~q;
 endmodule
 
 module async_counter #(parameter width = 4, max_value = 10)
-  (output [width-1:0] q, cnt_max, input enb, rst_s, ck);
+  (output [width-1:0] q, cnt_max, input enb, rst_s, clk);
   
   genvar i;
   generate
     for (i=0; i < width; i=i+1)
       if (i == 0)
-        d_ff u0 (.q(q[i]), .qb(), .clk(ck && enb), .clrn(rst_s), .d(1'b1 ^ q[i]));
+        d_ff u0 (.q(q[i]), .qb(), .clk(clk && enb), .clrn(rst_s), .d(1'b1 ^ q[i]));
       else
         d_ff ui (.q(q[i]), .qb(), .clk(q[i-1]), .clrn(rst_s), .d(1'b1 ^ q[i]));
   endgenerate
-
+  
   assign cnt_max = &q;
 
 endmodule
